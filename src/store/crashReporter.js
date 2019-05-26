5b4cd005abd01fe0b5d3c6baca17ac53134130a6
Patrick Brandt <patrick@fullstory.com>
2019-05-26 09:26:05 -0400

    adding some notes to myself

--- src/store/crashReporter.js
+++ src/store/crashReporter.js
@@ -1,5 +1,8 @@
 import { doError } from '../actions/error';
 
+// NOTE: dispatching from within this middleware could cause an infinite error handling loop
+// if the action handler for the error type (where Sentry and FS log errors) thrown an error as well
+// a other option that I'm mulling is to simply attempt to record the error and then have the execution stop
 const crashReporter = store => next => action => {
   // we got a thunk
   if (typeof action === 'function') {
@@ -7,14 +10,17 @@ const crashReporter = store => next => action => {
       try {
         await fn(dispatch, getState, extraArgument);
       } catch (e) {
-        console.log(`in wrapped action: ${e}`);
         dispatch(doError(e));
       }
     }
     return next(wrapAction(action));
   }
   
-  return next(action);
+  try {
+    return next(action);
+  } catch (e) {
+    store.dispatch(doError(e));
+  }
 };
 
 export default crashReporter;
